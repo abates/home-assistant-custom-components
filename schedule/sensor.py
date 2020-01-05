003d91d0cd6b9c864dd4671c5f36b6705c129606
Andrew Bates <abates@omeganetserv.com>
2020-01-05 10:14:46 -0500

    Added value templates
    Added ability for date and time values for the schedule slots
    to be derived from templates

--- schedule/sensor.py
+++ schedule/sensor.py
@@ -13,43 +13,59 @@
 # limitations under the License.
 """Platform for sensor integration."""
 
-from datetime import time, timedelta
+from datetime import datetime, date, time, timedelta
 
-from voluptuous import Datetime, Required, Schema
+import voluptuous as vol
 
-from homeassistant.const import ATTR_NAME, ATTR_TIME
+from homeassistant.const import ATTR_NAME, ATTR_DATE, ATTR_TIME
 from homeassistant.core import callback
-from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import async_track_point_in_utc_time
 from homeassistant.util import dt as dt_util
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        Required(ATTR_NAME): str,
-        Required("schedule"): [
-            Schema(
-                {
-                    Required(ATTR_NAME): str,
-                    Required(ATTR_TIME): Datetime(format="%M:%S"),
-                }
-            )
-        ],
-    }
+ATTR_DATE_TEMPLATE = f"{ATTR_DATE}_template"
+ATTR_TIME_TEMPLATE = f"{ATTR_TIME}_template"
+
+TIME_SCHEMA = vol.All(
+    vol.Schema(
+        {
+            vol.Required(ATTR_NAME): str,
+            vol.Exclusive(ATTR_TIME, "time"): vol.Datetime(format="%M:%S"),
+            vol.Exclusive(ATTR_TIME_TEMPLATE, "time"): cv.template,
+        }
+    ),
+    cv.has_at_least_one_key(ATTR_TIME, ATTR_TIME_TEMPLATE),
 )
 
+DATE_SCHEMA = vol.All(
+    vol.Schema(
+        {
+            vol.Required(ATTR_NAME): str,
+            vol.Exclusive(ATTR_DATE, "date"): vol.Datetime(format="%m/%d"),
+            vol.Exclusive(ATTR_DATE_TEMPLATE, "date"): cv.template,
+        }
+    ),
+    cv.has_at_least_one_key(ATTR_DATE, ATTR_DATE_TEMPLATE),
+)
 
+SCHEDULE_SCHEMA = vol.Any([TIME_SCHEMA], [DATE_SCHEMA])
+
+PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend(
+    {vol.Required(ATTR_NAME): str, vol.Required("schedule"): SCHEDULE_SCHEMA}
+)
+
+# TODO: update this so that the interval reflects day or time
 def _get_next_interval():
     """Determine the next time this sensor should be updated."""
     interval = 60
     now = dt_util.utcnow()
     timestamp = int(dt_util.as_timestamp(now))
     delta = interval - (timestamp % interval)
-    print(f"Now: {now} delta: {delta}\n")
     return now + timedelta(seconds=delta)
 
 
-def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the sensor platform."""
     device = ScheduleSensor(hass, config[ATTR_NAME], config["schedule"])
     async_track_point_in_utc_time(
@@ -58,24 +74,97 @@ def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     async_add_entities([device])
 
 
+DATE_FORMATS = [
+    "%m/%d",
+    "%Y/%m/%d",
+    "%Y-%m-%d",
+    "%Y-%m-%d %H:%M:%S",
+    "%Y-%m-%d %H:%M:%S.%f",
+]
+TIME_FORMATS = ["%H:%M", "%H:%M:%S", "%Y-%m-%d %H:%M:%S", "%Y-%m-%d %H:%M:%S.%f"]
+
+
+def parse(input, formats):
+    for fmt in formats:
+        try:
+            return datetime.strptime(input, fmt)
+        except ValueError:
+            pass
+
+    raise ValueError(f"{input} is not a recognized date/time")
+
+
+def parse_date(input):
+    "Parse a string into a date object."
+    return parse(input, DATE_FORMATS).date()
+
+
+def parse_time(input):
+    return parse(input, TIME_FORMATS).time()
+
+
 class _SchedSlot:
-    def __init__(self, name, hhmm):
-        self.name = name
-        parts = hhmm.split(":")
-        self._time = time(int(parts[0]), int(parts[1]))
+    def __init__(self, hass, config):
+        self.name = config[ATTR_NAME]
+        self._date = None
+        self._date_template = None
+        self._time = None
+        self._time_template = None
+
+        if ATTR_DATE in config:
+            self._date = parse_date(config[ATTR_DATE])
+
+        if ATTR_DATE_TEMPLATE in config:
+            self._date_template = config[ATTR_DATE_TEMPLATE]
+            self._date_template.hass = hass
+
+        if ATTR_TIME in config:
+            self._time = parse_time(config[ATTR_TIME])
+
+        if ATTR_TIME_TEMPLATE in config:
+            self._time_template = config[ATTR_TIME_TEMPLATE]
+            self._time_template.hass = hass
+
+    @property
+    def date(self):
+        if self._date is None and self._date_template is None:
+            return None
+
+        if self._date_template is None:
+            _date = self._date
+            _date = date(
+                dt_util.as_local(dt_util.now()).year, self._date.month, self._date.day
+            )
+
+            return _date
+
+        return parse_date(self._date_template.async_render())
+
+    @property
+    def time(self):
+        if self._time_template is None:
+            return self._time
+
+        return self._time_template.async_render().time()
 
     @property
     def start(self):
         """Get the start time of this slot."""
-        return self._time
+        return self.date or self.time
 
     def after(self, date_time):
         """Determine if this slot comes after the given time."""
-        return date_time.time() < self._time
+        if self.date is None:
+            return date_time.time() < self.time
+
+        return date_time.date() < self.date
 
     def active_at(self, date_time):
         """Determine if this slot is active (start time is prior to given time)."""
-        return self._time <= date_time.time()
+        if self.date is None:
+            return self.time <= date_time.time()
+
+        return self.date <= date_time.date()
 
 
 class ScheduleSensor(Entity):
@@ -88,7 +177,7 @@ class ScheduleSensor(Entity):
         self._state = None
         self._schedule = []
         for slot in schedule:
-            self._add_slot(slot[ATTR_NAME], slot[ATTR_TIME])
+            self._add_slot(slot)
 
         self._update_internal_state(dt_util.utcnow())
 
@@ -117,8 +206,8 @@ class ScheduleSensor(Entity):
         self._state = "unknown"
         return self
 
-    def _add_slot(self, name, hhmm):
-        self._schedule.append(_SchedSlot(name, hhmm))
+    def _add_slot(self, config):
+        self._schedule.append(_SchedSlot(self.hass, config))
         self._schedule.sort(key=lambda slot: slot.start, reverse=True)
 
     @callback
--- schedule/sensor_test.py
+++ schedule/sensor_test.py
@@ -16,35 +16,193 @@
 from unittest import TestCase
 from unittest.mock import MagicMock
 from homeassistant.util import dt as dt_util
-from .sensor import _get_next_interval, _SchedSlot, ScheduleSensor
-from datetime import datetime
+from .sensor import (
+    _get_next_interval,
+    _SchedSlot,
+    ScheduleSensor,
+    SCHEDULE_SCHEMA,
+    DATE_SCHEMA,
+    TIME_SCHEMA,
+    parse_date,
+    parse_time,
+)
+from datetime import datetime, date, time
+import voluptuous as vol
 
-# schedule = importlib.import_module("custom_components.schedule")
+
+class TestDateTimeParsing(TestCase):
+    def test_parse_date(self):
+        tests = [
+            {"parser": parse_date, "input": "10/21", "want": date(1900, 10, 21)},
+            {"parser": parse_date, "input": "2011/10/21", "want": date(2011, 10, 21)},
+            {"parser": parse_date, "input": "2012-10-21", "want": date(2012, 10, 21)},
+            {
+                "parser": parse_date,
+                "input": "2013-10-21 10:04:59.934104",
+                "want": date(2013, 10, 21),
+            },
+            {"parser": parse_date, "input": "10-21", "wantException": ValueError},
+            {"parser": parse_time, "input": "11:30", "want": time(11, 30)},
+            {"parser": parse_time, "input": "12:30:40", "want": time(12, 30, 40)},
+            {
+                "parser": parse_time,
+                "input": "2013-10-21 10:04:59",
+                "want": time(10, 4, 59),
+            },
+        ]
+
+        for test in tests:
+            if "wantException" in test:
+                with self.assertRaises(test["wantException"]):
+                    test["parser"](test["input"])
+            else:
+                try:
+                    got = test["parser"](test["input"])
+                    self.assertEqual(test["want"], got)
+                except ValueError:
+                    self.fail(f"Failed to parse {test['input']}")
 
 
-def dt(h, m):
-    return datetime(2010, 1, 1, h, m, 0)
+class TestSensorConfig(TestCase):
+    def test_invalid_schedule(self):
+        tests = [
+            {
+                "name": "test 1",
+                "input": [
+                    {"name": "d1", "date": "01/01"},
+                    {"name": "t2", "time": "02/01"},
+                ],
+            },
+        ]
+
+        for test in tests:
+            with self.assertRaises(vol.MultipleInvalid):
+                SCHEDULE_SCHEMA(test["input"])
+
+    def test_valid_schedule(self):
+        tests = [
+            {
+                "name": "test 1",
+                "input": [
+                    {"name": "t1", "time": "01:00"},
+                    {"name": "t2", "time": "02:00"},
+                ],
+            },
+            {
+                "name": "test 2",
+                "input": [
+                    {"name": "d1", "date": "01/01"},
+                    {"name": "d2", "date": "02/01"},
+                ],
+            },
+            {
+                "name": "test 3",
+                "input": [
+                    {"name": "d1", "date": "01/01"},
+                    {"name": "d2", "date_template": "{{ '02/01' }}"},
+                ],
+            },
+        ]
+
+        for test in tests:
+            try:
+                SCHEDULE_SCHEMA(test["input"])
+            except vol.MultipleInvalid as err:
+                name = test["name"]
+                self.fail(f"{name} failed to validate data: {err}")
+
+    def test_date_time_invalid(self):
+        tests = [
+            {"schema": DATE_SCHEMA, "input": {"name": "test 1"}},
+            {"schema": TIME_SCHEMA, "input": {"name": "test 1"}},
+            {
+                "schema": TIME_SCHEMA,
+                "input": {"name": "test 1", "time_template": "{{foo}"},
+            },
+        ]
+
+        for test in tests:
+            with self.assertRaises(vol.MultipleInvalid):
+                test["schema"](test["input"])
+
+    def test_date_time_valid(self):
+        tests = [
+            {"schema": DATE_SCHEMA, "input": {"name": "test 1", "date": "01/21"}},
+            {"schema": TIME_SCHEMA, "input": {"name": "test 1", "time": "01:21"}},
+            {
+                "schema": DATE_SCHEMA,
+                "input": {"name": "test 1", "date_template": "{{ '01/21' }}"},
+            },
+            {
+                "schema": TIME_SCHEMA,
+                "input": {"name": "test 1", "time_template": "{{ '01:21' }}"},
+            },
+        ]
+        for test in tests:
+            try:
+                test["schema"](test["input"])
+            except vol.MultipleInvalid:
+                self.fail("Failed to validate data")
 
 
 class TestScheduleSlot(TestCase):
+    def time(self, hour, minute):
+        return datetime(2010, 1, 1, hour, minute, 0)
+
+    def date(self, month, day):
+        return datetime(2010, month, day, 0, 0, 0)
+
+    def setUp(self):
+        self.as_local = dt_util.as_local
+        dt_util.as_local = MagicMock(return_value=datetime(2010, 1, 1, 0, 0, 0))
+
+    def tearDown(self):
+        dt_util.as_local = self.as_local
+
     def test_after(self):
-        self.assertTrue(_SchedSlot("", "01:00").after(dt(0, 0)))
-        self.assertFalse(_SchedSlot("", "01:00").after(dt(1, 0)))
+        self.assertTrue(
+            _SchedSlot(None, {"name": "", "time": "01:00"}).after(self.time(0, 0))
+        )
+        self.assertFalse(
+            _SchedSlot(None, {"name": "", "time": "01:00"}).after(self.time(1, 0))
+        )
+        self.assertTrue(
+            _SchedSlot(None, {"name": "", "date": "12/1"}).after(self.date(11, 1))
+        )
+        self.assertFalse(
+            _SchedSlot(None, {"name": "", "date": "11/1"}).after(self.date(12, 1))
+        )
 
     def test_active_at(self):
-        self.assertTrue(_SchedSlot("", "00:00").active_at(dt(0, 0)))
-        self.assertTrue(_SchedSlot("", "01:00").active_at(dt(2, 0)))
+        self.assertTrue(
+            _SchedSlot(None, {"name": "", "time": "00:00"}).active_at(self.time(0, 0))
+        )
+        self.assertTrue(
+            _SchedSlot(None, {"name": "", "time": "01:00"}).active_at(self.time(2, 0))
+        )
+        self.assertTrue(
+            _SchedSlot(None, {"name": "", "date": "11/1"}).active_at(self.date(11, 1))
+        )
 
 
 class TestScheduleSensor(TestCase):
-    def test_get_next_interval(self):
+    def time(self, hour, minute):
+        return datetime(2010, 1, 1, hour, minute, 0)
+
+    def setUp(self):
+        self.utcnow = dt_util.utcnow
         dt_util.utcnow = MagicMock(return_value=datetime(2010, 1, 1, 0, 0, 30))
+
+    def tearDown(self):
+        dt_util.utcnow = self.utcnow
+
+    def test_get_next_interval(self):
         self.assertEqual(
-            _get_next_interval(), datetime(2010, 1, 1, 0, 1, 0), "Incorrect interval"
+            _get_next_interval(), datetime(2010, 1, 1, 0, 1, 0), "Incorrect interval",
         )
         dt_util.utcnow = MagicMock(return_value=datetime(2010, 1, 1, 0, 0, 59))
         self.assertEqual(
-            _get_next_interval(), datetime(2010, 1, 1, 0, 1, 0), "Incorrect interval"
+            _get_next_interval(), datetime(2010, 1, 1, 0, 1, 0), "Incorrect interval",
         )
 
     def test_update_state(self):
@@ -58,8 +216,8 @@ class TestScheduleSensor(TestCase):
             ],
         )
 
-        self.assertEqual(schedule._update_internal_state(dt(2, 0)).state, "t2")
-        self.assertEqual(schedule._update_internal_state(dt(3, 0)).state, "t3")
-        self.assertEqual(schedule._update_internal_state(dt(1, 0)).state, "t1")
-        self.assertEqual(schedule._update_internal_state(dt(0, 0)).state, "t3")
+        self.assertEqual(schedule._update_internal_state(self.time(2, 0)).state, "t2")
+        self.assertEqual(schedule._update_internal_state(self.time(3, 0)).state, "t3")
+        self.assertEqual(schedule._update_internal_state(self.time(1, 0)).state, "t1")
+        self.assertEqual(schedule._update_internal_state(self.time(0, 0)).state, "t3")
 
